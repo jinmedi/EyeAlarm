bc890e8a999c83ed019ca835b554f76bc8ed45fb
jinmedi <jinmedi@hanmail.net>
2022-03-01 18:51:15 +0900

    Initial Commit

---
+++ EyeAlarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EyeAlarm
+//
+//  Created by 김종엽 on 2022/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
